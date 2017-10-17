ec8c7cd02615b78587ffea6b4e69bf8906f6bbb8
Michael <mthomas22@gmail.com>
2017-10-17 09:00:31 -0400

    adding trapezoid area

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,11 +14,25 @@ namespace ConsoleApp1
             //users choice of shape
             string shape;
             bool input = true;
+
             //triangle variables
             double triangleBaseOutput = 0;
             double triangleHeightOutput = 0;
             string triangleChoice;
             double triangleArea;
+
+            //rectangle variables
+            double rectangleWidthOutput = 0;
+            double rectangleHeightOutput = 0;
+            string rectangleChoice;
+            double rectangleArea;
+
+            //trapezoid variables
+            double firstTrapezoidWidthOutput = 0;
+            double secondTrapezoidWidthOutput = 0;
+            double trapezoidHeightOutput = 0;
+            string trapezoidChoice;
+            double trapezoidArea;
 
             do
             {
@@ -32,6 +46,7 @@ namespace ConsoleApp1
 
                 Console.Clear();
 
+                //Triangle Shape
                 if (shape == "1")
                 {
                     do
@@ -111,6 +126,109 @@ namespace ConsoleApp1
                     }
                     while (triangleChoice != "4");
                     Console.Clear();
+                }
+
+                //Rectangle Shape
+                if (shape == "2")
+                {
+                    do
+                    {
+                        Console.Clear();
+                        //gather information to get the area of a triangle
+                        Console.WriteLine("To get the area of your rectangle we need the width and height of the rectangle.");
+
+                        Console.WriteLine("1. Enter the width.");
+
+                        Console.WriteLine("2. Enter the height.");
+
+                        //show user the current value of rectangle width
+                        if (rectangleWidthOutput != 0)
+                        {
+                            Console.WriteLine("\nYour current width is {0}", rectangleWidthOutput);
+                        }
+                        //show user the current value of rectangle height
+                        if (rectangleHeightOutput != 0)
+                        {
+                            Console.WriteLine("Your current height is {0}", rectangleHeightOutput);
+                        }
+
+                        Console.WriteLine("3. Calculate the area of your rectangle.");
+
+                        Console.WriteLine("4. Go back");
+
+                        rectangleChoice = Console.ReadLine();
+
+                        Console.Clear();
+
+                        //user enters the width of the rectangle
+                        if (rectangleChoice == "1")
+                        {
+
+                            Console.WriteLine("Please enter the width of your rectangle");
+                            //checks to make sure user entered a number
+                            input = double.TryParse(Console.ReadLine(), out rectangleWidthOutput);
+
+                            while (input == false)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Please enter a number value for the width");
+                                Console.WriteLine("Please enter the width of your rectangle");
+                                input = double.TryParse(Console.ReadLine(), out rectangleWidthOutput);
+                            }
+                            Console.Clear();
+                        }
+
+                        //user enters the height of the rectangle
+                        if (rectangleChoice == "2")
+                        {
+
+                            Console.WriteLine("Please enter the height of your rectangle");
+                            //checks to make sure user entered a number
+                            input = double.TryParse(Console.ReadLine(), out rectangleHeightOutput);
+
+                            while (input == false)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Please enter a number value for the height");
+                                Console.WriteLine("Please enter the height of your rectangle");
+                                input = double.TryParse(Console.ReadLine(), out rectangleHeightOutput);
+                            }
+                            Console.Clear();
+                        }
+
+                        //calculate area of rectangle for the user
+                        if (rectangleChoice == "3")
+                        {
+                            Console.Clear();
+                            rectangleArea = rectangleHeightOutput * rectangleWidthOutput;
+                            Console.WriteLine("Your rectangles's area is {0}. Press enter to continue", rectangleArea);
+                            Console.ReadLine();
+                        }
+
+                    }
+                    while (rectangleChoice != "4");
+                    Console.Clear();
+                }
+
+                //Trapezoid Shape
+                if (shape == "3")
+                {
+                    do
+                    {
+                        Console.Clear();
+                        //gather information to get area of the trapezoid
+                        Console.WriteLine("To get the area of your trapezoid we need the two widths and the height of the trapezoid.");
+                        Console.WriteLine("1. First Width");
+                        Console.WriteLine("2. Second Width");
+                        Console.WriteLine("3. Height");
+
+                        //show current values of widths and height
+                        if(firstTrapezoidWidthOutput != 0)
+                        {
+                            Console.WriteLine("")
+                        }
+
+                    }
                 }
             }
             while (shape != "6");
